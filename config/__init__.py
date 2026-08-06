98bf404518abd9cdaee3bbdc5c6761d5d5e540e3
wirawan1923-rgb <wirawan1923@gmail.com>
2026-08-06 14:41:57 +0700

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,20 @@
+"""
+Configuration Package
+"""
+
+from config.settings import *
+
+__all__ = [
+    'APP_NAME',
+    'APP_VERSION',
+    'API_URL',
+    'HEADERS',
+    'REQUEST_TIMEOUT',
+    'RETRY_ATTEMPTS',
+    'RETRY_DELAY',
+    'DEFAULT_STATION_ID',
+    'DEFAULT_DAYS_BACK',
+    'DEFAULT_UPDATE_INTERVAL',
+    'DATA_DIR',
+    'CONFIG_FILE'
+]
